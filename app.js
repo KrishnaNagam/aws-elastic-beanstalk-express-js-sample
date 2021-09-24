0d06408c14307514cc239fb2d15bcb41d8bb872b
Krishna Nagam <krishnamkrishna26@gmail.com>
2021-09-24 13:11:35 +0000

    added test url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
